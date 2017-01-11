426dd66e0a44483ad3227bd2a7b3258a05dc3465
Hokuten85 <kellygrantleanna@gmail.com>
2017-01-11 17:26:57 -0600

    Treasure Casket experiementation

--- sql/TableBackup.bat
+++ sql/TableBackup.bat
@@ -5,27 +5,28 @@ cd "B:\FFIvalice\Database Backups"
 mkdir %mydate%
 cd %mydate%
 
-"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" -u ***REMOVED*** -p***REMOVED*** dspdb auction_house > .\auction_house.sql
-"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" -u ***REMOVED*** -p***REMOVED*** dspdb chars > .\chars.sql
-"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" -u ***REMOVED*** -p***REMOVED*** dspdb accounts > .\accounts.sql
-"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" -u ***REMOVED*** -p***REMOVED*** dspdb accounts_banned > .\accounts_banned.sql
-"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" -u ***REMOVED*** -p***REMOVED*** dspdb char_effects > .\char_effects.sql
-"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" -u ***REMOVED*** -p***REMOVED*** dspdb char_equip > .\char_equip.sql
-"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" -u ***REMOVED*** -p***REMOVED*** dspdb char_exp > .\char_exp.sql
-"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" -u ***REMOVED*** -p***REMOVED*** dspdb char_inventory > .\char_inventory.sql
-"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" -u ***REMOVED*** -p***REMOVED*** dspdb char_jobs > .\char_jobs.sql
-"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" -u ***REMOVED*** -p***REMOVED*** dspdb char_look > .\char_look.sql
-"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" -u ***REMOVED*** -p***REMOVED*** dspdb char_pet > .\char_pet.sql
-"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" -u ***REMOVED*** -p***REMOVED*** dspdb char_pet_name > .\char_pet_name.sql
-"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" -u ***REMOVED*** -p***REMOVED*** dspdb char_points > .\char_points.sql
-"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" -u ***REMOVED*** -p***REMOVED*** dspdb char_profile > .\char_profile.sql
-"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" -u ***REMOVED*** -p***REMOVED*** dspdb char_skills > .\char_skills.sql
-"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" -u ***REMOVED*** -p***REMOVED*** dspdb char_spells > .\char_spells.sql
-"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" -u ***REMOVED*** -p***REMOVED*** dspdb char_stats > .\char_stats.sql
-"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" -u ***REMOVED*** -p***REMOVED*** dspdb char_storage > .\char_storage.sql
-"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" -u ***REMOVED*** -p***REMOVED*** dspdb char_vars > .\char_vars.sql
-"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" -u ***REMOVED*** -p***REMOVED*** dspdb char_weapon_skill_points > .\char_weapon_skill_points.sql
-"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" -u ***REMOVED*** -p***REMOVED*** dspdb conquest_system > .\conquest_system.sql
-"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" -u ***REMOVED*** -p***REMOVED*** dspdb delivery_box > .\delivery_box.sql
-"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" -u ***REMOVED*** -p***REMOVED*** dspdb linkshells > .\linkshells.sql
-"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" -u ***REMOVED*** -p***REMOVED*** dspdb > .\dspdb.sql
+"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" --hex-blob -u ***REMOVED*** -p***REMOVED*** --default-character-set=utf8 --result-file=auction_house.sql dspdb auction_house
+"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" --hex-blob -u ***REMOVED*** -p***REMOVED*** --default-character-set=utf8 --result-file=chars.sql dspdb chars
+"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" --hex-blob -u ***REMOVED*** -p***REMOVED*** --default-character-set=utf8 --result-file=accounts.sql dspdb accounts
+"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" --hex-blob -u ***REMOVED*** -p***REMOVED*** --default-character-set=utf8 --result-file=accounts_banned.sql dspdb accounts_banned
+"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" --hex-blob -u ***REMOVED*** -p***REMOVED*** --default-character-set=utf8 --result-file=char_effects.sql dspdb char_effects
+"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" --hex-blob -u ***REMOVED*** -p***REMOVED*** --default-character-set=utf8 --result-file=char_equip.sql dspdb char_equip
+"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" --hex-blob -u ***REMOVED*** -p***REMOVED*** --default-character-set=utf8 --result-file=char_exp.sql dspdb char_exp
+"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" --hex-blob -u ***REMOVED*** -p***REMOVED*** --default-character-set=utf8 --result-file=char_inventory.sql dspdb char_inventory
+"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" --hex-blob -u ***REMOVED*** -p***REMOVED*** --default-character-set=utf8 --result-file=char_jobs.sql dspdb char_jobs
+"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" --hex-blob -u ***REMOVED*** -p***REMOVED*** --default-character-set=utf8 --result-file=char_look.sql dspdb char_look
+"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" --hex-blob -u ***REMOVED*** -p***REMOVED*** --default-character-set=utf8 --result-file=char_pet.sql dspdb char_pet
+"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" --hex-blob -u ***REMOVED*** -p***REMOVED*** --default-character-set=utf8 --result-file=char_pet_name.sql dspdb char_pet_name
+"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" --hex-blob -u ***REMOVED*** -p***REMOVED*** --default-character-set=utf8 --result-file=char_points.sql dspdb char_points
+"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" --hex-blob -u ***REMOVED*** -p***REMOVED*** --default-character-set=utf8 --result-file=char_profile.sql dspdb char_profile
+"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" --hex-blob -u ***REMOVED*** -p***REMOVED*** --default-character-set=utf8 --result-file=char_skills.sql dspdb char_skills
+"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" --hex-blob -u ***REMOVED*** -p***REMOVED*** --default-character-set=utf8 --result-file=char_spells.sql dspdb char_spells
+"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" --hex-blob -u ***REMOVED*** -p***REMOVED*** --default-character-set=utf8 --result-file=char_stats.sql dspdb char_stats
+"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" --hex-blob -u ***REMOVED*** -p***REMOVED*** --default-character-set=utf8 --result-file=char_storage.sql dspdb char_storage
+"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" --hex-blob -u ***REMOVED*** -p***REMOVED*** --default-character-set=utf8 --result-file=char_vars.sql dspdb char_vars
+"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" --hex-blob -u ***REMOVED*** -p***REMOVED*** --default-character-set=utf8 --result-file=char_weapon_skill_points.sql dspdb char_weapon_skill_points
+"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" --hex-blob -u ***REMOVED*** -p***REMOVED*** --default-character-set=utf8 --result-file=conquest_system.sql dspdb conquest_system
+"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" --hex-blob -u ***REMOVED*** -p***REMOVED*** --default-character-set=utf8 --result-file=delivery_box.sql dspdb delivery_box
+"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" --hex-blob -u ***REMOVED*** -p***REMOVED*** --default-character-set=utf8 --result-file=linkshells.sql dspdb linkshells
+"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" --hex-blob -u ***REMOVED*** -p***REMOVED*** --default-character-set=utf8 --result-file=dspdb.sql dspdb
+"C:\Program Files\MySQL\MySQL Server 5.7\bin\mysqldump" --hex-blob -u ***REMOVED*** -p***REMOVED*** --default-character-set=utf8 --result-file=npc_list.sql dspdb npc_list
