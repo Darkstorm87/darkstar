a100c0f11a88e34a2fa35a070685885bec892026
Hokuten85 <kellygrantleanna@gmail.com>
2017-01-12 00:45:19 -0600

    Update TextIDs.lua

--- scripts/zones/Abdhaljs_Isle-Purgonorgo/TextIDs.lua
+++ scripts/zones/Abdhaljs_Isle-Purgonorgo/TextIDs.lua
@@ -1,5 +1,5 @@
 -- Variable TextID   Description text
-
+--
 -- General Texts
 ITEM_CANNOT_BE_OBTAINED = 6379; -- You cannot obtain the item <item> come back again after sorting your inventory
           ITEM_OBTAINED = 6384; -- Obtained: <item>
