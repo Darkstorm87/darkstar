8cc11e24d0fb6aa3dac73af4cae5ec62bd4d6c3a
Wertlos <futurepop315@gmail.com>
2017-01-13 07:43:33 -0600

    Update Huge_Spider.lua

--- scripts/zones/Korroloka_Tunnel/mobs/Huge_Spider.lua
+++ scripts/zones/Korroloka_Tunnel/mobs/Huge_Spider.lua
@@ -20,19 +20,5 @@ end;
 -----------------------------------
 
 function onMobDespawn(mob)
-    local mobID = mob:getID();
-
-    if (Falcatus_Aranei_PH[mobID] ~= nil) then
-        local ToD = GetServerVariable("[POP]Falcatus_Aranei");
-        if (ToD <= os.time(t) and GetMobAction(Falcatus_Aranei) == 0) then
-            if (math.random(1,20) == 5) then
-                UpdateNMSpawnPoint(Falcatus_Aranei);
-                GetMobByID(Falcatus_Aranei):setRespawnTime(GetMobRespawnTime(mobID));
-                SetServerVariable("[PH]Falcatus_Aranei", mobID);
-                DeterMob(mobID, true);
-            end
-        end
-    end
-
 end;
 
