bfe093bca18e20fc1860c096643447c9bf0c9f0d
Wertlos <futurepop315@gmail.com>
2017-01-13 06:26:35 -0600

    Update Voluptuous_Vivian.lua

--- scripts/zones/The_Boyahda_Tree/mobs/Voluptuous_Vivian.lua
+++ scripts/zones/The_Boyahda_Tree/mobs/Voluptuous_Vivian.lua
@@ -28,15 +28,4 @@ end;
 -----------------------------------
 
 function onMobDespawn(mob)
-
-    -- Set VV's ToD
-    SetServerVariable("[POP]Voluptuous_Vivian", os.time(t) + math.random(57600,86400)); -- 16-24 hours
-    DeterMob(mob:getID(), true);
-
-    -- Set PH back to normal, then set to respawn spawn
-    local PH = GetServerVariable("[PH]Voluptuous_Vivian");
-    SetServerVariable("[PH]Voluptuous_Vivian", 0);
-    DeterMob(PH, false);
-    GetMobByID(PH):setRespawnTime(GetMobRespawnTime(PH));
-
 end;
