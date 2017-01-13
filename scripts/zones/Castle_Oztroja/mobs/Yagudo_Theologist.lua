4c9996a91b46f04b68e4fc0f44e5e2ec9acfbe0a
Wertlos <futurepop315@gmail.com>
2017-01-13 07:15:59 -0600

    Update Yagudo_Theologist.lua

--- scripts/zones/Castle_Oztroja/mobs/Yagudo_Theologist.lua
+++ scripts/zones/Castle_Oztroja/mobs/Yagudo_Theologist.lua
@@ -17,20 +17,5 @@ end;
 -----------------------------------
 
 function onMobDespawn(mob)
-
-    local mobID = mob:getID();
-    if (Moo_Ouzi_the_Swiftblade_PH[mobID] ~= nil) then
-
-        local ToD = GetServerVariable("[POP]Moo_Ouzi_the_Swiftblade");
-        if (ToD <= os.time(t) and GetMobAction(Moo_Ouzi_the_Swiftblade) == 0) then
-            if (math.random(1,20) == 5) then
-                UpdateNMSpawnPoint(Moo_Ouzi_the_Swiftblade);
-                GetMobByID(Moo_Ouzi_the_Swiftblade):setRespawnTime(GetMobRespawnTime(mobID));
-                SetServerVariable("[PH]Moo_Ouzi_the_Swiftblade", mobID);
-                DeterMob(mobID, true);
-            end
-        end
-    end
-
 end;
 
