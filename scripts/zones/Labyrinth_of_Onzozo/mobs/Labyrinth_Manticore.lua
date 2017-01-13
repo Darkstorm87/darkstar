fbf3c7bb2cc143e9791c9db15827f9ad74e5e6af
Wertlos <futurepop315@gmail.com>
2017-01-13 10:39:05 -0600

    Update Labyrinth_Manticore.lua

--- scripts/zones/Labyrinth_of_Onzozo/mobs/Labyrinth_Manticore.lua
+++ scripts/zones/Labyrinth_of_Onzozo/mobs/Labyrinth_Manticore.lua
@@ -22,17 +22,4 @@ end;
 -----------------------------------
 
 function onMobDespawn(mob)
-    local mobID = mob:getID();
-
-    if (Narasimha_PH[mobID] ~= nil) then
-        local ToD = GetServerVariable("[POP]Narasimha");
-        if (ToD <= os.time(t) and GetMobAction(Narasimha) == 0) then
-            if (math.random(1,20) == 5) then
-                UpdateNMSpawnPoint(Narasimha);
-                GetMobByID(Narasimha):setRespawnTime(GetMobRespawnTime(mobID));
-                SetServerVariable("[PH]Narasimha", mobID);
-                DeterMob(mobID, true);
-            end
-        end
-    end
 end;
