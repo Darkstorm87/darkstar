15f9093a774acc05e974c137edc219a003620415
Hokuten85 <kellygrantleanna@gmail.com>
2017-05-18 23:17:14 -0500

    healing adjustment and settings changes

--- scripts/globals/effects/healing.lua
+++ scripts/globals/effects/healing.lua
@@ -23,16 +23,18 @@ end;
 function onEffectTick(target,effect)
 
     local healtime = effect:getTickCount();
+	local baseHp = math.max(target:getMainLvl(), 10);
+	local baseMp = math.max(math.floor(target:getMainLvl()/2), 10);
 
     if (healtime > 1) then
         -- curse II also known as "zombie"
         if (not(target:hasStatusEffect(EFFECT_DISEASE)) and target:hasStatusEffect(EFFECT_PLAGUE) == false and target:hasStatusEffect(EFFECT_CURSE_II) == false) then
             local healHP = 0;
             if (target:getContinentID() == 1 and target:hasStatusEffect(EFFECT_SIGNET)) then
-                healHP = 10+(3*math.floor(target:getMainLvl()/10))+(healtime-2)*(1+math.floor(target:getMaxHP()/300))+(target:getMod(MOD_HPHEAL));
+                healHP = baseHp+(3*math.floor(target:getMainLvl()/10))+(healtime-2)*(1+math.floor(target:getMaxHP()/300))+(target:getMod(MOD_HPHEAL));
             else
                 target:addTP(HEALING_TP_CHANGE);
-                healHP = 10+(healtime-2)+(target:getMod(MOD_HPHEAL));
+                healHP = baseHp+(healtime-2)+(target:getMod(MOD_HPHEAL));
             end
 
             target:addHP(healHP);
@@ -41,7 +43,7 @@ function onEffectTick(target,effect)
          -- Each rank of Clear Mind provides +3 hMP (via MOD_MPHEAL)
          -- Each tic of healing should be +1mp more than the last
          -- Clear Mind III increases this to +2, and Clear Mind V to +3 (via MOD_CLEAR_MIND)
-            target:addMP(12+((healtime-2) * (1+target:getMod(MOD_CLEAR_MIND)))+(target:getMod(MOD_MPHEAL)));
+            target:addMP(baseMp+((healtime-2) * (1+target:getMod(MOD_CLEAR_MIND)))+(target:getMod(MOD_MPHEAL)));
         end
     end
 
