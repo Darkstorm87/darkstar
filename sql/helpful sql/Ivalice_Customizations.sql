b5fb867bb3f44d5834635cb4d4331e15935a70dc
Hokuten85 <kellygrantleanna@gmail.com>
2019-11-12 22:54:01 -0600

    shell and protect duration and fixing augmenting drops

--- sql/helpful sql/Ivalice_Customizations.sql
+++ sql/helpful sql/Ivalice_Customizations.sql
@@ -113,6 +113,54 @@ AND mfs.systemid NOT IN (19)
 AND mg.groupid = mg.groupid;
 
 -- mob_droplist
+-- newDrops is for situations where dropid is repeated across NMs that we have special augment drops for, so break it up into different dropids, then add the augment drops
+DROP TABLE IF EXISTS `newDrops`;
+CREATE TABLE newDrops (
+	groupid int,
+	zoneid int,
+	oldDropId int,
+	newDropId int AUTO_INCREMENT,
+	PRIMARY KEY (newDropId)
+);
+-- Set @varMax = (SELECT MAX(dropid)+1 FROM mob_droplist);
+ALTER TABLE newDrops AUTO_INCREMENT = 3138 XXX; -- BREAK HERE BECAUSE NEED TO CHECK FOR NEW MAX DROP ID
+
+INSERT INTO newDrops (groupid, zoneid, oldDropId)
+SELECT DISTINCT mg.groupid, mg.zoneid, mg.dropid
+FROM mob_spawn_points msp
+INNER JOIN mob_groups mg
+	ON msp.groupid = mg.groupid
+	AND mg.zoneid = ((msp.mobid >> 12) & 0xFFF)
+INNER JOIN mob_pools mp
+	ON mg.poolid = mp.poolid
+INNER JOIN zone_settings zs
+	ON mg.zoneid = zs.zoneid
+INNER JOIN (
+	SELECT dropid
+    FROM mob_groups
+    GROUP BY dropid
+    HAVING count(1) > 1
+    ) bd
+    ON mg.dropid = bd.dropid
+WHERE mp.mobType & 0x02
+AND NOT mp.mobType & (0x10 | 0x20)
+AND zs.zonetype NOT IN (4,5,6)
+AND mg.dropid > 0
+AND mg.minlevel DIV 10 < 10;
+
+INSERT INTO mob_droplist
+select newDropId, dropType, md.groupId, groupRate, itemId, itemRate
+from mob_droplist md
+INNER JOIN newDrops nd
+	ON md.dropid = nd.oldDropId;
+    
+UPDATE mob_groups mg
+INNER JOIN newDrops nd
+	ON mg.groupid = nd.groupid
+    AND mg.zoneid = nd.zoneid
+    AND mg.dropid = nd.oldDropId
+SET mg.dropid = nd.newDropId;
+
 INSERT INTO mob_droplist
 SELECT DISTINCT md.dropId, 0, 0, 1000,
 	CASE itemId
