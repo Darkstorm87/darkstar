dcbd3f65b60732967c2702f2ba05d172df1598a2
Hokuten85 <kellygrantleanna@gmail.com>
2019-12-01 17:28:25 -0600

    Update spike spells to be castable on other people

--- scripts/globals/effects/weight.lua
+++ scripts/globals/effects/weight.lua
@@ -8,6 +8,12 @@ require("scripts/globals/status")
 
 function onEffectGain(target, effect)
     target:addMod(dsp.mod.MOVE, -effect:getPower())
+	
+	if target:isMob() then
+		local power = math.min(10, target:getStat(dsp.mod.EVA))
+		effect:setSubPower(power)
+		target:delMod(dsp.mod.EVA, power)
+	end
 end
 
 function onEffectTick(target, effect)
@@ -15,4 +21,9 @@ end
 
 function onEffectLose(target, effect)
     target:delMod(dsp.mod.MOVE, -effect:getPower())
+	
+	if target:isMob() then
+		local power = effect:getSubPower()
+		target:addMod(dsp.mod.EVA, power)
+	end
 end
